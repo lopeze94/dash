7af63ed7470e9eac36ec2819b90a581142f9e60c
sonderspace <lopezefrain5894@gmail.com>
2018-02-14 12:57:36 -0600

    Update optimization.py

--- dash_project/optimization.py
+++ dash_project/optimization.py
@@ -5,7 +5,7 @@ from mozscape import Mozscape
 import time
 # an Instance of the Optimization_Return_Functions class
 
-
+# arguments are required to use this tool
 moz_client = Mozscape('', '')
 
 """Through constructors for each selection option -- we can use same url
@@ -74,94 +74,3 @@ def return_score_normalized_moz_rank(url):
     data = moz_client.urlMetrics(url, cols=65536)
     return data['pmrp']
     # this returns only normal moz rank for root domain
-
-#  for assign_and_return_competitor_normalized_root_domain_moz_trust()
-#  we are
-#  only returning normalized score
-
-
-"""
-
-These are now inside Optimization_Return_Functions.py
-
-Begin Return Statements With ['keys']
-
-NOTE: These statements are under revision for modular..
-They require being executed through the
-above corresponding functions ONLY
-
-
-
-
-def return_competitor_amount_external_links(data):
-    return data['ued']
-    # Returns number of external links to the URL,
-    # including nofollowed links
-
-
-def return_competitor_amount_root_domain_links(data):
-    return data['ped']
-    #  Returns number of external links to the root domain,
-    #  including nofollowed links
-
-
-def return_competitor_amount_c_blocks(data):
-    return data['pib']  # int
-    #  Returns number of links from the same C class IP addresses.
-
-
-def return_competitor_score_domain_authority(data):
-    return data['pda']  # int 0/100 - best to display it this way
-    #  Returns normalized 100-point score representing the
-    #  likelihood of a domain to rank well in search engine results
-
-
-def return_competitor_crawler_timestamp(data):
-    return data['ulc']
-    #  Returns time and date on which Mozscape last
-    #  crawled the URL, returned in Unix epoch format
-
-
-def return_competitor_total_root_domain_links(data):
-    return data['puid']  # int
-    #  The total number of links, including internal and nofollow links,
-    #  to the root domain of the URL
-
-
-def return_competitor_normalized_root_domain_score(data):
-    return data['ptrp']  # number
-    # The MozTrust of the root domain of the URL,
-    # in both the normalized 10-point score (ptrp)
-
-
-def return_competitor_normalized_root_domain_moz_trust(data):
-    return data['utrp']
-
-
-
-POTENTIAL PROBLEMS
-
-These keys ['ddd'] can be switched the variables instead -- possibly
-passed by an external widget where we can switch it --
-var = function() which returns a code through a Selector!
-data[var]
-During the switch on a selector - a different key is passed, or
-a differnt set of keys to be passed then assigned locally
-
-this will be necessary to use less code to reach into several
-different columns within one metric.
-
-This may be a second parameter to be passed in a function's
-last name(var, var,..)
-
-
-
-POTENTIAL PROBLEMS
-
-Perhaps the selector will assign already stored variables for the cols Calls
-this way a pair ors set of pairs, of a cols bit flag and keys are passed to
-reduce necessary code
-
-
-May be able to do assignments and returns within only 2 functions max
-"""
